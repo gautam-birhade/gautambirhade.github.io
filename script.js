26ef6228cb614c816d21d8cafd1654fd1045db43
gautam-birhade <97681635+gautam-birhade@users.noreply.github.com>
2022-01-13 19:42:58 +0530

    1/13/2022

---
+++ script.js
@@ -0,0 +1,18 @@
+var typed = new Typed('.typing', {
+    strings: [" Web Devloper", "Web Designer"],
+    typeSpeed: 80,
+    backSpeed: 80,
+    loop: true,
+    smartBackspace: false,
+    delay: 0
+});
+
+// var nav = document.querySelector('nav');
+
+// window.addEventListener('scroll', function() {
+//     if (window.pageYOffset > 100) {
+//         nav.classList.add('bg-dark', 'shadow');
+//     } else {
+//         nav.classList.remove('bg-dark', 'shadow');
+//     }
+// });
